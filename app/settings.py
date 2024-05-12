44d06d4311fec5c078e0ea8deae8cededac92adf
Leonardo <ep.leonardorocha@gmail.com>
2024-05-11 21:51:02 -0300

    fim_modulo_9

--- app/settings.py
+++ app/settings.py
@@ -42,7 +42,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['app/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from cars.models import Car
 
-# Create your views here.
+
+def cars_view(request):
+    cars = Car.objects.all().order_by('-model')
+    search = request.GET.get('search')
+
+    if search:
+        cars = Car.objects.filter(model__icontains=search).order_by('model')
+    return render(request,
+                  'cars.html',
+                  context={'cars': cars})
