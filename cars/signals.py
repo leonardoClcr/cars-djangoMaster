7e891ee73671fa783416696b842cd41425de9d68
leonardo <ep.leonardorocha@gmail.com>
2024-05-19 14:03:33 -0300

    postgresql_instalado

--- cars/signals.py
+++ cars/signals.py
@@ -2,7 +2,6 @@ from django.db.models.signals import pre_save, pre_delete, post_save, post_delet
 from django.dispatch import receiver
 from cars.models import Car, CarInventory
 from django.db.models import Sum
-from openai_api.client import get_car_ai_bio
 
 
 def car_inventory_update():
